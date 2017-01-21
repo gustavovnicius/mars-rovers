d3b40ca124a5f44d629f6e697f4deb58e128d204
Gustavo Bastos <me@gustavovinicius.com>
2017-01-21 19:09:45 -0200

    feature: Put movement into the rover

--- lib/rover.ex
+++ lib/rover.ex
@@ -1,2 +1,9 @@
 defmodule Rover do
+  def move(position, plateau, "M") do
+    Movement.walk(position, plateau)
+  end
+
+  def move(%{x: x, y: y, direction: facing}, _plateau, rotate_to) when rotate_to != "M" do
+    %{x: x, y: y, facing: Movement.rotate_to(rotate_to, facing)}
+  end
 end
--- spec/lib/rover_spec.exs
+++ spec/lib/rover_spec.exs
@@ -1,4 +1,28 @@
 defmodule RoverSpec do
   use ESpec
 
+  context "walking" do
+    let position: %{}, plateau: %{}
+
+    it "should walk" do
+      allow(Movement).to accept(:walk, fn(_, _) -> nil end)
+
+      Rover.move(position, plateau, "M")
+
+      expect(Movement) |> to(accepted(:walk, [position, plateau]))
+    end
+
+    context "rotating" do
+      let facing_north: "N", rotate_to_left: "L"
+      let position: %{x: 0, y: 0, direction: facing_north}, plateau: %{}
+
+      it "should rotate" do
+        allow(Movement).to accept(:rotate_to, fn(_, _) -> nil end)
+
+        Rover.move(position, plateau, rotate_to_left)
+
+        expect(Movement) |> to(accepted(:rotate_to, [rotate_to_left, facing_north]))
+      end
+    end
+  end
 end
